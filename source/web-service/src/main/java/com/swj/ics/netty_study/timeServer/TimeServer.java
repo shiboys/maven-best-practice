0985dedfe3c30018243c5b1fdf14217b683dc777
shiboys <shiboys@126.com>
2018-06-12 23:49:45 +0800

    Netty_Time_Server demo 1

--- source/web-service/src/main/java/com/swj/ics/netty_study/timeServer/TimeServer.java
+++ source/web-service/src/main/java/com/swj/ics/netty_study/timeServer/TimeServer.java
@@ -22,19 +22,32 @@ public class TimeServer {
     }
 
     static void startServer(int port) {
+        //创建2个NioEventLoopGroup实例
+        //NioEventGroup是个线程组，它包含一组NIO线程，专门用来网络事件的处理，实际上它们就是Reactor线程组。
+        //这里创建2个的原因是一个用于服务器端接受客户端的连接，另一个用于进行SocketChannel的网络读写。
+        //
         NioEventLoopGroup watcherGroup = new NioEventLoopGroup();
         NioEventLoopGroup workerGroup = new NioEventLoopGroup();
+        //ServerBootstrap对象是Netty用于启动NIO服务端的辅助启动类，目的是降低服务端的开发复杂度。
         ServerBootstrap sbp = new ServerBootstrap();
+        //这里调用group方法，将2个NIO线程组当做人参传递到ServerBootStrap中。接着设置channel为NIOServerSocketChannel。它的功能对应于JDK的 NIO中的
+        //ServerSocketChannel类。
         sbp.group(watcherGroup,workerGroup)
                 .channel(NioServerSocketChannel.class)
-                .option(ChannelOption.SO_BACKLOG,1024)
-                .childHandler(new ChildChannelHandler());
-
+                .option(ChannelOption.SO_BACKLOG,1024) //这里配置TCP参数，此处将backlog设置为1024,关于backlog的参数注释如下：
+                .childHandler(new ChildChannelHandler()); //这里绑定IO时间处理类ChildChannelHandler,它的作用类似于Reactor模式中的Handler类，
+        // 主要用于处理网络I/O事件，例如记录日志，对消息进行编解码
+                /*
+                *ChannelOption.SO_BACKLOG对应的是tcp/ip协议listen函数中的backlog参数，函数listen(int socketfd,int backlog)用来初始化服务端可连接队列，
+　　　　服务端处理客户端连接请求是顺序处理的，所以同一时间只能处理一个客户端连接，多个客户端来的时候，
+服务端将不能处理的客户端连接请求放在队列中等待处理，backlog参数指定了队列的大小 
+                * */
         try {
-            //绑定端口，等待绑定成功。
+            //绑定端口，进行监听，等待绑定成功。调用sync同步阻塞方法等待绑定操作的完成。完成之后，Netty会返回一个ChannelFuture,
+            //它的功能类似于JDK的java.util.concurrent.Future,主要用于异步操作的通知回调。
             ChannelFuture channelFuture = sbp.bind(port).sync();
 
-            //等待服务端的监听端口关闭
+            //等待服务端的监听端口关闭，等待服务端链路关闭之后，main函数才会退出
             channelFuture.channel().closeFuture().sync();
 
         } catch (InterruptedException e) {
--- source/web-service/src/main/java/com/swj/ics/netty_study/timeServer/TimeServerHandler.java
+++ source/web-service/src/main/java/com/swj/ics/netty_study/timeServer/TimeServerHandler.java
@@ -33,11 +33,16 @@ public class TimeServerHandler extends ChannelHandlerAdapter {
 
     @Override
     public void channelReadComplete(ChannelHandlerContext ctx) throws Exception {
+        //flush的作用是将发送队列中的消息写入SocketChannel中并发给对方，从性能角度考虑,为了防止频繁的唤醒Selector进行消息发送，
+        //Netty的write方法不是直接将消息写入SocketChannel中，调用write方法只是将待发送的消息发送到缓冲数组中，再通过调用flush方法，将发送缓冲区中的
+        //消息全部写入到SocketChannel中。
+        
         ctx.flush();
     }
 
     @Override
     public void exceptionCaught(ChannelHandlerContext ctx, Throwable cause) throws Exception {
-        super.exceptionCaught(ctx, cause);
+        cause.printStackTrace();
+        ctx.close();
     }
 }
