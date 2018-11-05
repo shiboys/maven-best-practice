df86c31e71af9f2e0dc8d1b991c5ca0a62b4152c
shiboys <shiboys@126.com>
2018-11-05 23:35:11 +0800

    marshalling2

--- source/web-service/src/main/java/com/swj/ics/netty_study/JbossMarshalling/JBossMarshallingFactory.java
+++ source/web-service/src/main/java/com/swj/ics/netty_study/JbossMarshalling/JBossMarshallingFactory.java
@@ -1,7 +1,10 @@
 package com.swj.ics.netty_study.JbossMarshalling;
 
+import io.netty.handler.codec.marshalling.DefaultMarshallerProvider;
 import io.netty.handler.codec.marshalling.DefaultUnmarshallerProvider;
+import io.netty.handler.codec.marshalling.MarshallerProvider;
 import io.netty.handler.codec.marshalling.MarshallingDecoder;
+import io.netty.handler.codec.marshalling.MarshallingEncoder;
 import io.netty.handler.codec.marshalling.UnmarshallerProvider;
 import org.jboss.marshalling.MarshallerFactory;
 import org.jboss.marshalling.Marshalling;
@@ -12,7 +15,9 @@ import org.jboss.marshalling.MarshallingConfiguration;
  * date 2018/11/5 下午5:12
  */
 public class JBossMarshallingFactory {
-    public static MarshallingDecoder buildMarshalingDecorder() {
+    //Netty的 marshalling编码和解码支持半包和粘包的处理，
+    //对开发者而言，只需要正确的将编码器和解码器加入到ChannelPipeline中个，就能实现对Marshalling序列化的支持。
+    public static MarshallingDecoder buildMarshallingDecoder() {
 
         MarshallerFactory factory = Marshalling.getProvidedMarshallerFactory("serial");
         MarshallingConfiguration configuration = new MarshallingConfiguration();
@@ -20,5 +25,14 @@ public class JBossMarshallingFactory {
         UnmarshallerProvider provider = new DefaultUnmarshallerProvider(factory,configuration);
         MarshallingDecoder decoder = new MarshallingDecoder(provider,1024);
         return decoder;
+    }
+    
+    public static MarshallingEncoder buildMarshallingEncoder() {
+        MarshallerFactory factory = Marshalling.getProvidedMarshallerFactory("serial");
+        MarshallingConfiguration configuration = new MarshallingConfiguration();
+        configuration.setVersion(5);
+        MarshallerProvider provider =new DefaultMarshallerProvider(factory,configuration);
+        MarshallingEncoder encoder = new MarshallingEncoder(provider);
+        return encoder;
     }
 }
---
+++ source/web-service/src/main/java/com/swj/ics/netty_study/JbossMarshalling/JbossMarshallingTest.java
@@ -0,0 +1,7 @@
+package com.swj.ics.netty_study.JbossMarshalling;
+
+/**
+ * Created by swj on 2018/11/5.
+ */
+public class JbossMarshallingTest {
+}
